73e60d9a737e0ee35bab17ef023dfac00f7e5dc6
ipfsnut <40326908+ipfsnut@users.noreply.github.com>
2025-04-07 13:25:08 -0500

    okay

--- inevitable/src/components/BookAssets.tsx
+++ inevitable/src/components/BookAssets.tsx
@@ -14,6 +14,7 @@ export function BookAssets() {
           Official Website
         </a>
       </div>
+      <meta name='fc:frame' content='{"version":"next","imageUrl":"https://oiwymzxncdvipdvgbycw.supabase.co/storage/v1/object/public/public/collections/cover_art_originals/InevitableCover.png","aspectRatio":"3:2","button":{"title":"INEVITABLE","action":{"type":"launch_frame","name":"INEVITABLE","url":"https://getinevitable.com","splashImageUrl":"https://oiwymzxncdvipdvgbycw.supabase.co/storage/v1/object/public/public/collections/cover_art_originals/InevitableCover.png","splashBackgroundColor":"#000000"}}}' />
     </div>
   );
 }
