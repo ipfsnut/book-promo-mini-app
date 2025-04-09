dfdcb2d85c0d947be1d42cea72f6c16f5abad829
ipfsnut <40326908+ipfsnut@users.noreply.github.com>
2025-04-09 11:19:54 -0500

    new db needs to be live; token swaps do NOT - yet.

---
+++ inevitable/src/constants/addresses.ts
@@ -0,0 +1,8 @@
+// Correct Uniswap V3 SwapRouter address on Base
+export const UNISWAP_V3_ROUTER = '0x2626664c2603336E57B271c5C0b26F421741e481' as `0x${string}`;
+
+// WETH address on Base
+export const WETH_ADDRESS = '0x4200000000000000000000000000000000000006' as `0x${string}`;
+
+// Pool fee (0.3% = 3000)
+export const POOL_FEE = 3000;
